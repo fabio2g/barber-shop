664175c145cc24f1bc12b950d2706eac2544ad7e
fabio-sv <fabio_dev@outlook.com>
2022-02-17 20:31:27 -0300

    mobile

---
+++ js/script.js
@@ -0,0 +1,27 @@
+let time = 5000
+let currentImageIndex = 0
+let images = document.querySelectorAll("#carrosel img")
+console.log(images)
+let max = images.length
+
+function nextImage() {
+
+  images[currentImageIndex].classList.remove("selected")
+
+  currentImageIndex++
+
+  if(currentImageIndex >= max) {
+    currentImageIndex = 0
+  }
+
+  images[currentImageIndex].classList.add("selected")
+
+}
+function start() {
+  setInterval(() => {
+    nextImage()
+  }, time)
+
+}
+
+window.addEventListener("load", start)
